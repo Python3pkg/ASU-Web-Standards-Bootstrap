7603cdf2aa95d570701ccf71dff2b8d5f62a1a72
Ivan Montiel <idmontie@asu.edu>
2014-10-08 16:37:40 -0700

    Refactored styles into individual files.

--- test/travis.rb
+++ test/travis.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
-result = `sass main.scss built.css`
+result = `sass bootstrap-asu.scss built-1.css`
 raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('built.css')
+raise "When compiled the module should output some CSS" unless File.exists?('built-1.css')
+
+result = `sass bootstrap-asu-theme-base.scss built-2.css`
+raise result unless $?.to_i == 0
+raise "When compiled the module should output some CSS" unless File.exists?('built-2.css')
+
 puts "Regular compile worked successfully"
