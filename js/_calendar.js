cb66bde1a51fb9ccc9d9c001489b6342f313112f
Ivan Montiel <idmontie@asu.edu>
2014-12-12 10:41:10 -0700

    calendar popover is now a full fledge plugin

--- js/_calendar.js
+++ js/_calendar.js
@@ -8,34 +8,102 @@
 +function ($) {
   'use strict';
 
-  $.fn.calendarPopover = function () {
-    function generateICS ( data ) {
-      // TODO issue #9
-      return '' + data
-    }
+  // POPOVER PUBLIC CLASS DEFINITION
+  // ===============================
 
-    this.popover( {
-      animation: true,
-      html: true,
-      placement: 'bottom',
-      trigger: 'click',
-      content: function () {
-        // Grab the datetime from the content
-        var dateTime = $(this).find('time').attr('datetime') || ''
-        var eventName = $(this).attr('title') || ''
-        var timeZone = $(this).find('time').attr('data-timezone') || ''
-        var filename = $(this).attr('data-filename') || 'invite.ics'
-
-        var ics = generateICS( {
-          dateTime : dateTime,
-          eventName : eventName,
-          timeZone : timeZone
-        } )
-
-        return '<a download="' + filename + '" href="' + 'data:text/plain;charset=utf-8,' + encodeURIComponent(ics) + '">Add this event to your calendar<\/a>';
-      }
-    } )
+  var CalendarPopover = function (element, options) {
+    this.init('popover', element, options)
   }
 
-  $('.calendarPopover').calendarPopover();
+  if (! $.fn.popover) throw new Error('Calendar Popover requires popover')
+
+  CalendarPopover.VERSION = '0.0.1'
+
+  CalendarPopover.DEFAULTS = $.extend({}, $.fn.popover.Constructor.DEFAULTS, {
+    animation: true,
+    html: true,
+    placement: 'bottom',
+    trigger: 'click',
+    content: function () {
+
+      // Grab the datetime from the content
+      var dateTime = $(this).find('time').attr('datetime') || ''
+      var eventName = $(this).attr('title') || ''
+      var timeZone = $(this).find('time').attr('data-timezone') || ''
+      var filename = $(this).attr('data-filename') || 'invite.ics'
+
+      // TODO test
+      var ics = this.generateICS( {
+        dateTime : dateTime,
+        eventName : eventName,
+        timeZone : timeZone
+      } )
+
+      return '<a download="' + filename + '" href="' + 'data:text/plain;charset=utf-8,' + encodeURIComponent(ics) + '">Add this event to your calendar<\/a>';
+    }
+  })
+
+  // NOTE: POPOVER EXTENDS popover.js
+  // ================================
+
+  CalendarPopover.prototype = $.extend({}, $.fn.popover.Constructor.prototype)
+
+  CalendarPopover.prototype.constructor = CalendarPopover
+
+  CalendarPopover.prototype.getDefault = function () {
+    return CalendarPopover.DEFAULTS
+  }
+
+  CalendarPopover.prototype.generateICS = function ( data ) {
+    // TODO issue #9
+    return '' + data
+  }
+
+
+  // CALENDAR POPOVER PLUGIN DEFINITION
+  // ==================================
+
+  function Plugin(option) {
+    return this.each(function () {
+      var $this    = $(this)
+      var data     = $this.data('bs.calendarPopover')
+      var options  = typeof option == 'object' && option
+      var selector = options && options.selector
+
+      if (! data && option == 'destroy')
+        return
+      if (selector) {
+        if (! data)
+          $this.data('bs.calendarPopover', (data = {}))
+        if (! data[selector])
+          data[selector] = new CalendarPopover(this, options)
+      } else {
+        if (! data)
+          $this.data('bs.calendarPopover', (data = new CalendarPopover(this, options)))
+      }
+
+      if (typeof option == 'string')
+        data[option]()
+    })
+  }
+
+  var old = $.fn.calendarPopover
+
+  $.fn.calendarPopover            = Plugin
+  $.fn.calendarPopover.Constructor = CalendarPopover
+
+  // CALENDAR POPOVER NO CONFLICT
+  // ============================
+
+  $.fn.calendarPopover.noConflict = function () {
+    $.fn.calendarPopover = old
+    return this
+  }
+
+  /*
+   * Run on page load (bootstrap it)
+   */
+  $(document).ready( function () {
+    $('.calendarPopover').calendarPopover()
+  } )
 } (jQuery);
--- test/qunit/test/calendar-test.js
+++ test/qunit/test/calendar-test.js
@@ -1,9 +1,33 @@
 $(function () {
   'use strict';
 
-  module('calendar')
+  module('calendar plugin')
 
   test('should be defined on jquery object', function () {
     ok($(document.body).calendarPopover, 'popover method is defined')
   })
+
+  module('calendar', {
+    setup: function () {
+      // Run all tests in noConflict mode -- it's the only way to ensure that the plugin works in noConflict mode
+      $.fn.wsCalendarPopover = $.fn.calendarPopover.noConflict()
+    },
+    teardown: function () {
+      $.fn.calendarPopover = $.fn.wsCalendarPopover
+      delete $.fn.wsCalendarPopover
+    }
+  })
+
+  test('should provide no conflict', function () {
+    strictEqual($.fn.calendarPopover, undefined, 'popover was set back to undefined (org value)')
+  });
+
+  test('should return jquery collection containing the element', function () {
+    var $el = $('<div/>')
+    var $popover = $el.wsCalendarPopover()
+    ok($popover instanceof $, 'returns jquery collection')
+    strictEqual($popover[0], $el[0], 'collection contains element')
+  })
+
+  // TODO
 })
