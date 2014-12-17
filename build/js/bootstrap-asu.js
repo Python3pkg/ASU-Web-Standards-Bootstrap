3149fd8ca022e9ecab39eab2d3c223653e3f6b78
Ivan Montiel <idmontie@asu.edu>
2014-12-17 16:48:13 -0700

    upversion, v0.0.4

--- build/js/bootstrap-asu.js
+++ build/js/bootstrap-asu.js
@@ -43,16 +43,32 @@
 /*
  * Throttle the window resize event for performance and smoothness.
  */
-+function ( $, sr ) {
++function ($) {
   'use strict';
+
+  // SMARTRESIZE PUBLIC CLASS DEFINITION
+  // ===================================
+
+  var Smartresize = function (element, options) {
+    this.element = element
+    this.options = options
+    this._bounce = []
+  }
+
+  Smartresize.VERSION = '0.0.2'
+
+  Smartresize.prototype = {}
+
+  Smartresize.prototype.constructor = Smartresize
+
   /**
    * Debounce function
    * @author John Hann
    * @source http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
    */
-  var debounce = function ( func, threshold, execAsap ) {
+  Smartresize.prototype.debounce = function ( func, threshold, execAsap ) {
     var timeout
-    return function () {
+    var outer = function () {
       var obj = this
       var args = arguments
 
@@ -71,18 +87,73 @@
 
       timeout = setTimeout( delayed, threshold || 500 )
     }
+
+    this._bounce.push({
+      bounce : outer,
+      callback : func
+    })
+
+    return outer
   }
 
   /**
-   * Smart Resize Function
-   * @author Paul Irish
-   * @source http://www.paulirish.com/2009/throttled-smartresize-jquery-event-handler/
+   * Unbinds specific handlers or if handler is null or undefined, will
+   * unbind ALL handlers
+   *
+   * @param  {function} handler the callback
+   * @return {$}         returns the element for chainability
    */
-  jQuery.fn[sr] = function (fn) {
-    return fn ? this.bind( 'resize', debounce( fn ) ) : this.trigger( sr )
+  Smartresize.prototype.unbind = function (handler) {
+    for (var i = 0; i < this._bounce.length; i++ ) {
+      if (handler == null || handler == this._bounce[i].callback) {
+        $(this.element).unbind( 'resize', this._bounce[i].bounce )
+      }
+    }
+    return $(this.element)
   }
 
-} (jQuery, 'smartresize')
+  // SMARTRESIZE POPOVER PLUGIN DEFINITION
+  // ==================================
+
+  function Plugin(option, handler) {
+    var $this    = $(this)
+    var data     = $this.data('bs.smartresize')
+    var options  = typeof option == 'object' && option
+    var selector = options && options.selector
+
+    if (! data && option == 'destroy')
+        return
+    if (selector) {
+      if (! data)
+        $this.data('bs.smartresize', (data = {}))
+      if (! data[selector])
+        data[selector] = new Smartresize(this, options)
+    } else {
+      if (! data)
+        $this.data('bs.smartresize', (data = new Smartresize(this, options)))
+    }
+
+    if (typeof option == 'string')
+      data[option](handler)
+    else {
+      return option ? this.bind( 'resize', data.debounce( option ) ) : this.trigger( 'smartresize' )
+    }
+  }
+
+  var old = $.fn.smartresize
+
+  $.fn.smartresize             = Plugin
+  $.fn.smartresize.Constructor = Smartresize
+
+  // SMARTRESIZE POPOVER NO CONFLICT
+  // ============================
+
+  $.fn.smartresize.noConflict = function () {
+    $.fn.smartresize = old
+    return this
+  }
+
+} (jQuery)
 
 /* ========================================================================
  * Web Standards: calendar.js v0.0.2
@@ -175,7 +246,7 @@
 
   var old = $.fn.calendarPopover
 
-  $.fn.calendarPopover            = Plugin
+  $.fn.calendarPopover             = Plugin
   $.fn.calendarPopover.Constructor = CalendarPopover
 
   // CALENDAR POPOVER NO CONFLICT
