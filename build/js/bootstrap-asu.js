536538b09a0de5b9b4f19bb82a16ecba733b781d
Ivan Montiel <idmontie@asu.edu>
2014-12-19 10:00:05 -0700

    initial calendar downloads for calendar widgets

--- build/js/bootstrap-asu.js
+++ build/js/bootstrap-asu.js
@@ -184,21 +184,73 @@
     content: function () {
 
       // Grab the datetime from the content
-      var dateTime = $(this).find('time').attr('datetime') || ''
-      var eventName = $(this).attr('title') || ''
-      var timeZone = $(this).find('time').attr('data-timezone') || ''
-      var filename = $(this).attr('data-filename') || 'invite.ics'
+      var dateStart   = $(this).find('time').attr('datetime') || ''
+      var dateEnd     = $(this).find('time').attr('datetime') || ''
+      var eventName   = $(this).attr('title') || ''
+      var timeZone    = $(this).find('time').attr('data-timezone') || 'Z'
+      var filename    = $(this).attr('data-filename') || 'invite.ics'
+      var subject     = $(this).attr('data-subject') || ''
+      var description = $(this).attr('data-description') || ''
+      var location    = $(this).attr('data-location') || ''
+      var mailto      = $(this).attr('data-mailto') || 'me@email.com'
 
       // TODO test
-      var ics = this.generateICS( {
-        dateTime : dateTime,
-        eventName : eventName,
-        timeZone : timeZone
+      var ics = CalendarPopover.ICS( {
+        timeZone : timeZone,
+        message : description,
+        mailto : mailto,
+        dateStart : dateStart,
+        dateEnd : dateEnd,
+        location : location,
+        summary : eventName + ( subject === '' ? '' : ' : ' + subject )
       } )
 
       return '<a download="' + filename + '" href="' + 'data:text/plain;charset=utf-8,' + encodeURIComponent(ics) + '">Add this event to your calendar<\/a>';
     }
   })
+
+  CalendarPopover.ICS = function ( data ) {
+    var dateStamp = Date.now()
+    var uid       = CalendarPopover.UID()
+    var company   = 'ASU'
+
+    var timeZone  = data.timeZone
+    var message   = data.message
+    var mailto    = data.mailto
+    var dateStart = data.dateStart
+    var dateEnd   = data.dateEnd
+    var location  = data.location
+    var summary   = data.summary
+
+    var icsMessage = 'BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//'
+    icsMessage += company
+    icsMessage += '//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:'
+    icsMessage += uid
+    icsMessage += '\nDTSTAMP:'
+    icsMessage += dateStamp + timeZone
+    icsMessage += '\nATTENDEE;CN='
+    icsMessage += message + ' '
+    icsMessage += ';RSVP=TRUE:MAILTO:'
+    icsMessage += mailto
+    icsMessage += '\nORGANIZER;CN=Me:MAILTO::'
+    icsMessage += mailto
+    icsMessage += '\nDTSTART:'
+    icsMessage += dateStart
+    icsMessage += '\nDTEND:'
+    icsMessage += dateEnd
+    icsMessage += '\nLOCATION:'
+    icsMessage += location
+    icsMessage += '\nSUMMARY:'
+    icsMessage += summary
+    icsMessage += '\nEND:VEVENT\nEND:VCALENDAR';
+
+    return icsMessage
+  }
+
+  CalendarPopover.UID = function () {
+    // Quick, short uid
+    return ('0000' + (Math.random() * Math.pow(36,4) << 0).toString(36)).slice(-4)
+  }
 
   // NOTE: POPOVER EXTENDS popover.js
   // ================================
@@ -210,12 +262,6 @@
   CalendarPopover.prototype.getDefault = function () {
     return CalendarPopover.DEFAULTS
   }
-
-  CalendarPopover.prototype.generateICS = function ( data ) {
-    // TODO issue #9
-    return '' + data
-  }
-
 
   // CALENDAR POPOVER PLUGIN DEFINITION
   // ==================================
@@ -235,12 +281,18 @@
         if (! data[selector])
           data[selector] = new CalendarPopover(this, options)
       } else {
+        if (! option)
+          option = CalendarPopover.DEFAULTS
+
         if (! data)
           $this.data('bs.calendarPopover', (data = new CalendarPopover(this, options)))
       }
 
       if (typeof option == 'string')
         data[option]()
+      else
+        data.$element.popover(option)
+
     })
   }
 
