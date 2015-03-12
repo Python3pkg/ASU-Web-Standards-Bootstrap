d6b148c7fa486633087a6c76f7184bb5a79e7db6
Ivan Montiel <idmontie@asu.edu>
2015-03-12 09:53:17 -0700

    fix navbar affix mobile

--- js/_sticky-nav.js
+++ js/_sticky-nav.js
@@ -8,6 +8,9 @@
 +function ($) {
   'use strict';
 
+  // TODO refactor this constant
+  var mobileWidth = 992
+
   $(function () {
     $('.navbar.navbar-ws').affix({
       offset: {
@@ -16,7 +19,9 @@
     }).each(function (i, e) {
       $(e).on('affix.bs.affix', function () {
         // Add the height of the navbar to the margin of the body
-        $('body').css('margin-top', $(e).outerHeight() + 'px');
+        if ( $( window ).width() >= mobileWidth ) {
+          $('body').css('margin-top', $(e).outerHeight() + 'px');
+        }
       }).on('affixed-top.bs.affix', function () {
         // Remove the height of the navbar to the margin of the body
         $('body').css('margin-top', '0px');
