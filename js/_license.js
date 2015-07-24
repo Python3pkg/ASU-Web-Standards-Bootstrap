ae37539bd8d8cd544d7fe4d77bf4decd44176bf9
Ivan Montiel <idmontie@asu.edu>
2015-07-24 10:02:38 -0700

    Documentation branch

--- js/_license.js
+++
@@ -1,7 +0,0 @@
-/**========================================================================
- * Web Standards: bootstrap-asu.js v0.1.8
- * ========================================================================
- * Copyright 2014 ASU
- * Licensed under MIT (https://github.com/gios-asu/ASU-Bootstrap-Addon/blob/master/LICENSE)
- * @license MIT
- * ======================================================================== */
