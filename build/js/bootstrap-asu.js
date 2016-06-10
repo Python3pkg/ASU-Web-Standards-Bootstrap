9ea5b0b807c866751311f1a4a3a150beb9b09d52
Arpit Mathur <arpitmathur0789@gmail.com>
2016-06-10 15:47:29 -0700

    Added the logic for dropdown menu click functionality - need modification

--- build/js/bootstrap-asu.js
+++ build/js/bootstrap-asu.js
@@ -510,6 +510,8 @@ case"millisecond":return Math.floor(24*b*60*60*1e3)+this._milliseconds;default:t
     } else {
       // Remove collapse open class
       $('.big-foot-nav').removeClass('in')
+      // Arpit Changes
+      $('.big-foot h2 span').removeClass('footer-caret-down')
     }
   }
 
@@ -517,11 +519,22 @@ case"millisecond":return Math.floor(24*b*60*60*1e3)+this._milliseconds;default:t
     if ( $(window).innerWidth() >= mobileWidth ) {
       // Don't collapse!
       e.preventDefault();
-
       var id = $(this).attr('data-target');
       $(id).addClass('in');
-
       return false;
+    } else {
+      var id1 = $(this).attr('data-target');
+      if ( $('.big-foot-nav').hasClass('in') ) {
+         // Arpit caret down remove on click
+        $('.big-foot h2[data-target="' + id1 + '"] span').removeClass('footer-caret-down')
+        // Arpit Changes for caret left
+        $('.big-foot h2[data-target="' + id1 + '"]  span').addClass('footer-caret')
+      } else {
+        // Arpit caret left remove on click
+        $('.big-foot h2[data-target="' + id1 + '"] span').removeClass('footer-caret')
+        // Arpit Changes for caret down
+        $('.big-foot h2[data-target="' + id1 + '"]  span').addClass('footer-caret-down')
+      }
     }
   });
 
--- js/_collapse-footer.js
+++ js/_collapse-footer.js
@@ -12,6 +12,8 @@
     } else {
       // Remove collapse open class
       $('.big-foot-nav').removeClass('in')
+      // Arpit Changes
+      $('.big-foot h2 span').removeClass('footer-caret-down')
     }
   }
 
@@ -19,11 +21,22 @@
     if ( $(window).innerWidth() >= mobileWidth ) {
       // Don't collapse!
       e.preventDefault();
-
       var id = $(this).attr('data-target');
       $(id).addClass('in');
-
       return false;
+    } else {
+      var id1 = $(this).attr('data-target');
+      if ( $('.big-foot-nav').hasClass('in') ) {
+         // Arpit caret down remove on click
+        $('.big-foot h2[data-target="' + id1 + '"] span').removeClass('footer-caret-down')
+        // Arpit Changes for caret left
+        $('.big-foot h2[data-target="' + id1 + '"]  span').addClass('footer-caret')
+      } else {
+        // Arpit caret left remove on click
+        $('.big-foot h2[data-target="' + id1 + '"] span').removeClass('footer-caret')
+        // Arpit Changes for caret down
+        $('.big-foot h2[data-target="' + id1 + '"]  span').addClass('footer-caret-down')
+      }
     }
   });
 
