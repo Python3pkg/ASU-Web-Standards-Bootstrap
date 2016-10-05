a4290050e17abfa9921674fbba9af24a59fc1eaa
Nathan Rollins <nathan.rollins@asu.edu>
2016-10-05 11:23:36 -0700

    Coding standards

--- js/_fastclick.js
+++ js/_fastclick.js
@@ -3,10 +3,10 @@
 
   if ( typeof FastClick == 'undefined' ) throw new Error( 'FastClick is required!' )
 
-  if ('addEventListener' in document) {
-      document.addEventListener('DOMContentLoaded', function() {
-          FastClick.attach(document.body);
-      }, false);
+  if ( 'addEventListener' in document ) {
+    document.addEventListener ( 'DOMContentLoaded', function () {
+      FastClick.attach ( document.body );
+    }, false );
   }
 
 } ();
